48433160d1c80b6a8f9d15ea9d733e6d4b669589
raychung0520 <raychung0520@gmail.com>
2020-06-08 07:22:54 -0700

    Greeting feature done

---
+++ greeting.js
@@ -0,0 +1,47 @@
+const form = document.querySelector(".js-form"); // input
+const input = form.querySelector("input");
+const greeting = document.querySelector(".js-greetings");
+
+const USER_LS = "currentUser",
+  SHOWING_ON = "showing";
+
+function saveName(name) {
+  localStorage.setItem(USER_LS, name);
+}
+
+function paintGreeting(text) {
+  //console.log(`${text}`);
+  console.log(text);
+  form.classList.remove(SHOWING_ON);
+  greeting.classList.add(SHOWING_ON);
+  greeting.innerHTML = `Hello ${text}`;
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = input.value;
+  saveName(currentValue);
+  paintGreeting(currentValue);
+}
+
+function askForName() {
+  form.classList.add(SHOWING_ON);
+  form.addEventListener("submit", handleSubmit);
+}
+
+function loadName() {
+  const currentUser = localStorage.getItem(USER_LS);
+  console.log(currentUser);
+
+  if (currentUser === null) {
+    askForName();
+  } else {
+    paintGreeting(currentUser);
+  }
+}
+
+function init() {
+  loadName();
+}
+
+init();
